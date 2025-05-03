2db4d10d68fdc4d90a5108c7a01553ad8df293c7
Ahmadreza Musavi <ahmadreza.musavi@email.kntu.ac.ir>
2025-05-03 22:12:52 +0330

    change database tables

--- postgres_code.sql
+++ postgres_code.sql
@@ -18,36 +18,38 @@ CREATE TYPE vehicle_type As ENUM('BUS', 'TRAIN', 'FLIGHT');
 
 CREATE TABLE vehicles (
     vehicle_id SERIAL PRIMARY KEY,
-    facility JSONB,
     vehicle_type vehicle_type NOT NULL
 );
 
 CREATE TABLE flights (
-    vehicle_id INTEGER PRIMARY KEY REFERENCES vehicles(vehicle_id) ON DELETE CASCADE,
+    vehicle_id INTEGER PRIMARY KEY REFERENCES vehicles(vehicle_id) ON DELETE CASCADE ON UPDATE CASCADE,
     airline_name VARCHAR(100) NOT NULL,
     flight_class VARCHAR(50) NOT NULL,
-    number_of_stop INTEGER CHECK (number_of_stop >= 0),
+    number_of_stop INTEGER CHECK (number_of_stop >= 0) DEFAULT 0,
     flight_code VARCHAR(50) NOT NULL,
     origin_airport VARCHAR(100) NOT NULL,
-    destination_airport VARCHAR(100) NOT NULL
+    destination_airport VARCHAR(100) NOT NULL,
+    facility JSONB
 );
 
 CREATE TABLE trains (
-    vehicle_id INTEGER PRIMARY KEY REFERENCES vehicles(vehicle_id) ON DELETE CASCADE,
+    vehicle_id INTEGER PRIMARY KEY REFERENCES vehicles(vehicle_id) ON DELETE CASCADE ON UPDATE CASCADE,
     train_stars INTEGER CHECK (train_stars > 0 AND train_stars < 6) NOT NULL,
-    choosing_a_closed_coupe BOOLEAN
+    choosing_a_closed_coupe BOOLEAN DEFAULT NO,
+    facility JSONB
 );
 
 CREATE TABLE buses (
-    vehicle_id INTEGER PRIMARY KEY REFERENCES vehicles(vehicle_id) ON DELETE CASCADE,
+    vehicle_id INTEGER PRIMARY KEY REFERENCES vehicles(vehicle_id) ON DELETE CASCADE ON UPDATE CASCADE,
     company_name VARCHAR(100)  NOT NULL,
     bus_type VARCHAR(50)  NOT NULL,
-    number_of_chairs INTEGER CHECK (number_of_chairs > 0)
+    number_of_chairs INTEGER CHECK (number_of_chairs > 0) DEFAULT 25,
+    facility JSONB
 );
 
 CREATE TABLE tickets (
     ticket_id SERIAL PRIMARY KEY,
-    vehicle_id INTEGER REFERENCES vehicles(vehicle_id) ON UPDATE CASCADE,
+    vehicle_id INTEGER REFERENCES vehicles(vehicle_id) ON DELETE SET NULL ON UPDATE CASCADE,
     origin VARCHAR(100) NOT NULL,
     destination VARCHAR(100) NOT NULL,
     date_and_time_of_departure TIMESTAMP NOT NULL,
@@ -60,10 +62,10 @@ CREATE TYPE reservation_status As ENUM('RESERVED', 'NOT_RESERVED');
 
 CREATE TABLE reservations (
     reservation_id SERIAL PRIMARY KEY,
-    username VARCHAR(50) REFERENCES users(username) ON UPDATE CASCADE ON DELETE CASCADE,
+    username VARCHAR(50) REFERENCES users(username) ON UPDATE CASCADE ON DELETE SET NULL,
     ticket_id INTEGER REFERENCES tickets(ticket_id) ON UPDATE CASCADE ON DELETE CASCADE,
     reservation_status reservation_status NOT NULL DEFAULT 'NOT_RESERVED',
-    date_and_time_of_reservation TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    date_and_time_of_reservation TIMESTAMP,
     reservation_seat INTEGER CHECK (reservation_seat > 0) NOT NULL
 );
 
@@ -72,8 +74,8 @@ CREATE TYPE payment_method As ENUM('CRYPTOCURRENCY', 'WALLET', 'CREDIT_CARD');
 
 CREATE TABLE payments (
     payment_id SERIAL PRIMARY KEY,
-    username VARCHAR(50) REFERENCES users(username) ON UPDATE CASCADE ON DELETE CASCADE,
-    reservation_id INTEGER REFERENCES reservations(reservation_id) ON UPDATE CASCADE,
+    username VARCHAR(50) REFERENCES users(username) ON UPDATE CASCADE ON DELETE SET NULL,
+    reservation_id INTEGER REFERENCES reservations(reservation_id) ON DELETE SET NULL ON UPDATE CASCADE,
     amount_paid INTEGER CHECK (amount_paid > 0),
     payment_status payment_status NOT NULL DEFAULT 'NOT_PAID',
     date_and_time_of_payment TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
@@ -86,7 +88,7 @@ CREATE TYPE report_status As ENUM('CHECKED', 'UNCHECKED');
 CREATE TABLE reports (
     report_id SERIAL PRIMARY KEY,
     username VARCHAR(50) REFERENCES users(username) ON UPDATE CASCADE ON DELETE CASCADE,
-    reservation_id INTEGER REFERENCES reservations(reservation_id) ON UPDATE CASCADE,
+    reservation_id INTEGER REFERENCES reservations(reservation_id) ON DELETE CASCADED ON UPDATE CASCADE,
     report_type report_type NOT NULL,
     report_text TEXT,
     report_status report_status NOT NULL DEFAULT 'UNCHECKED'
@@ -95,11 +97,11 @@ CREATE TABLE reports (
 CREATE TYPE operation_type As ENUM('BUY', 'CANCEL');
 
 CREATE TABLE reservations_history (
-    username VARCHAR(50) REFERENCES users(username) ON UPDATE CASCADE,
-    reservation_id INTEGER REFERENCES reservations(reservation_id) ON UPDATE CASCADE,
+    reservation_history_id SERIAL PRIMARY KEY,
+    username VARCHAR(50) REFERENCES users(username) ON DELETE SET NULL ON UPDATE CASCADE,
+    reservation_id INTEGER REFERENCES reservations(reservation_id) ON DELETE CASCADED ON UPDATE CASCADE,
     date_and_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    operation_type operation_type NOT NULL,
-    PRIMARY KEY (reservation_id, username, date_and_time)
+    operation_type operation_type NOT NULL
 );
 
 CREATE INDEX idx_tickets_vehicle_id ON tickets(vehicle_id);
