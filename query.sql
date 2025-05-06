febd8d92c5ad0ca0393dcb33c61c40e4c516cfae
AmirHossein9822 <amirhosseinb1606@gmail.com>
2025-05-06 10:49:20 +0330

    Add queries 6 to 10

--- query.sql
+++ query.sql
@@ -54,3 +54,62 @@ select u.name
 		on u.username  = p.username
 			order by p.date_and_time_of_payment desc
 				limit 1;
+
+
+--6
+select phone_number
+from users u
+         join payments p on p.username = u.username
+group by u.username
+having sum(p.amount_paid) > (select avg(total_paid)
+                             from (select sum(amount_paid) as total_paid
+                                   from payments
+                                   group by username) as user_totals);
+
+select email
+from users u
+         left join payments p on p.username = u.username
+group by u.username
+having sum(COALESCE(p.amount_paid, 0)) > (select avg(total_paid)
+                                          from (select sum(coalesce(p2.amount_paid, 0)) as total_paid
+                                                from users u2
+                                                         left join payments p2 on u2.username = p2.username
+                                                group by u2.username));
+
+--7
+select count(r.reservation_id), v.vehicle_type as type
+from reservations r
+         join tickets t
+              On t.ticket_id = r.ticket_id
+         join vehicles v
+              On v.vehicle_id = t.vehicle_id
+where r.reservation_status = 'RESERVED'
+group by v.vehicle_type;
+
+--8
+select name, count(r.reservation_id) as count_of_reservation
+from users u
+         join reservations r on r.username = u.username
+where DATE (r.date_and_time_of_reservation) >= CURRENT_DATE - interval '7 days'
+group by u.username
+order by count (r.reservation_id) desc limit 3;
+
+--9
+select l.city, count(r.reservation_id)
+from reservations r
+         join tickets t on t.ticket_id = r.ticket_id
+         join locations l on l.location_id = t.origin_location_id
+where l.province = 'Tehran'
+group by l.city;
+
+--10
+select u.username, l.city
+from users u
+         join reservations r on r.username = u.username
+         join tickets t on t.ticket_id = r.ticket_id
+         join locations l on l.location_id = t.origin_location_id
+where u.username = (select username
+                    from users u2
+                    order by u2.date_of_sign_in limit 1
+    );
+
